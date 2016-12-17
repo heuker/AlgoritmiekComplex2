bf7c63a1d6f2695edf11f9aeb44e5a7a832fd4ce
Sander Groot Wesseldijk <heukertje@gmail.com>
2016-12-17 13:17:33 +0100

    JavaDoc

--- src/Disk.java
+++ src/Disk.java
@@ -2,27 +2,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Created by Sander on 29-11-2016.
+ * Class that simulates a Disk with data, can write to and from
  */
 public class Disk {
     private ArrayList<Integer> data;
     private ArrayList<ArrayList<Integer>> outArray;
 
+    /**
+     * Constructor
+     * @param data the data to be sorted in runs
+     */
     public Disk(ArrayList data) {
         this.data = data;
         outArray = new ArrayList<>();
     }
 
+    /**
+     * Get the whole data ArrayList
+     * @return data
+     */
     public ArrayList<Integer> getData() {
         return data;
     }
 
+    /**
+     * Get the next value of the ArrayList and than delete it
+     * @return index 0 of data
+     */
     public int getNext(){
         int value = data.get(0);
         data.remove(0);
         return value;
     }
 
+    /**
+     * Returns an array of the first numbers
+     * @param amount the amount of numbers to be returned
+     * @return the returned numbers
+     */
     public int[] multipleAsArray(int amount){
         if (amount > data.size()){
             amount = data.size();
@@ -36,6 +53,10 @@ public class Disk {
         return multiple;
     }
 
+    /**
+     * Writes a new run to the out ArrayList
+     * @param numbers the run to be added (as ArrayList)
+     */
     public void writeRunToDisk(ArrayList<Integer> numbers){
         outArray.add(new ArrayList<>(numbers));
     }
--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,32 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Created by Sander on 29-11-2016.
+ * Main class that contains Replacement Selection
  */
 public class Main {
-
-    public static void main(String[] args) {
-        new Main().run();
-    }
-
+    //The RSHeap
     RSHeap RSHeap = new RSHeap(5);
 
     public void run() {
+        //Make a list of random numbers(Duplicates allowed)
         ArrayList<Integer> data = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < 50; i++){
+        for (int i = 0; i < 50; i++) {
             data.add(random.nextInt(100));
         }
 
+        //Fill a Disk with the random numbers and run ReplacementSelection
         Disk disk = new Disk(data);
-
-        getRuns(5, disk);
+        ReplacementSelection(5, disk);
     }
 
-    public void print() {
-        for (int i = 0; i < RSHeap.getHeap().length; i++) {
-            System.out.print(RSHeap.getHeap()[i]);
-        }
-        System.out.println();
-        System.out.println("Deadspace: " + RSHeap.getDeadSpaceSize());
-    }
-
-    public void getRuns(int runSize, Disk disk) {
+    /**
+     * RepalcementSelection algorithm that uses a RSHeap
+     *
+     * @param runSize the given runSize(Max memory)
+     * @param disk    The disk containing the data
+     */
+    public void ReplacementSelection(int runSize, Disk disk) {
         RSHeap heap = new RSHeap(runSize);
 
         //build heap
@@ -46,13 +41,14 @@ public class Main {
             lastAdded = heap.pop();
             run.add(lastAdded);
 
+            //get the next number
             int next = disk.getNext();
 
-            //If next fits in heap add it
+            //If next can be added to the heap add it
             if (next >= lastAdded) {
                 heap.insert(next);
 
-                //else increase deadspace and add the number to it
+                //else increase the deadspace and add the number to it
             } else {
                 heap.incrementDeadspace();
                 heap.insertInDeadSpace(next);
@@ -61,25 +57,33 @@ public class Main {
                 if (heap.getDeadSpaceSize() == runSize) {
                     heap.buildHeap(heap.getHeap());
 
-                    //write the run to Disk and clear it
+                    //write the run to Disk and than clear run
                     disk.writeRunToDisk(run);
                     run.clear();
                 }
             }
         }
+        //write remaining run to the disk
         disk.writeRunToDisk(run);
         run.clear();
 
-        //if there are numbers left in the deadspace than process them to
-        if (heap.getDeadSpaceSize() != 0){
-            heap.buildHeap(heap.getHeap());
-            for (int i = 0; i < heap.getHeap().length; i++){
-                run.add(heap.pop());
-            }
-            disk.writeRunToDisk(run);
+        //Progress the remaining numbers
+        heap.buildHeap(heap.getHeap());
+
+        //the heap is sorted so we dont have to check for deadspace
+        for (int i = 0; i < heap.getHeap().length; i++) {
+            run.add(heap.pop());
+            //Increase the deadspace so that the heap thinks there are less elements
+            heap.incrementDeadspace();
         }
+        disk.writeRunToDisk(run);
+
 
         //print the result
         disk.printResult();
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
     }
 }
